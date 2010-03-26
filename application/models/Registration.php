cba54d4de09ba02d34cd95e1d4c25cd385b424cd
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-26 12:35:50 +0100

    Update save registration

--- application/models/Registration.php
+++ application/models/Registration.php
@@ -12,34 +12,19 @@
  */
 class Registration extends BaseRegistration
 {
-
+    /**
+     * Save Registration
+     * @param <type> $array
+     */
     public static function saveRegistration($array)
     {
         $r  = new Registration();
-        /*
+        
         foreach ($array as $key => $value) {
-            echo $key."<br>";
             $field = (string) $key;
-            //$r->$field = $value;
-            
+            $r->$field = $value;            
         }
          
-         */
-        $r->name = $array['name'];
-        $r->surname = $array['surname'];
-        $r->email = $array['email'];
-        $r->country =  $array['country'];
-        $r->gar = $array['gar'];
-        $r->membershipnumber = $array['membershipnumber'];
-        $r->membershiptype =  $array['membershiptype'];
-        $r->recording = $array['recording'];
-        $r->wholeretreat = $array['wholeretreat'];
-        $r->datefrom = $array['datefrom'];
-        $r->dateto = $array['dateto'];
-        $r->children = $array['children'];
-        $r->childrencount = $array['childrencount'];
-        $r->eat = $array['eat'];
-
         ZFCore_Utils::log('Add registration: '.var_export($array, true));
         $r->save();        
     }
