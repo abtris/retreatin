6d14630a9511365332667a3113e32df37bdae75c
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-26 09:47:41 +0100

    Add schema and doctrine settings for basic models

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,41 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+    protected function _initDoctrine()
+    {
+        $cacheConfig = $this->getOption('cache');
+        $this->getApplication()->getAutoloader()
+                ->pushAutoloader(array('Doctrine', 'autoload'));
+        spl_autoload_register(array('Doctrine', 'modelsAutoload'));
+        $manager = Doctrine_Manager::getInstance();
+        $manager->setAttribute(Doctrine::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+        $manager->setAttribute(
+                Doctrine::ATTR_MODEL_LOADING,
+                Doctrine::MODEL_LOADING_CONSERVATIVE
+        );
+
+        $doctrine = $this->getOption('doctrine');
+        Doctrine::loadModels($doctrine['models_path']);
+        Doctrine::loadModels($doctrine['models_path']."/generated");
+        $conn = Doctrine_Manager::connection($doctrine['dsn'], 'doctrine');
+        if ($configCache['enabled']==1) {
+            $servers = array(
+                    'host' => 'localhost',
+                    'port' => 11211,
+                    'persistent' => true
+            );
+
+            $cacheDriver = new Doctrine_Cache_Memcache(array(
+                            'servers' => $servers,
+                            'compression' => false
+            ));
+            $conn->setAttribute(Doctrine::ATTR_RESULT_CACHE, $cacheDriver);
+        }
+        $conn->setAttribute(Doctrine::ATTR_USE_NATIVE_ENUM, true);
+        $conn->setCharset('utf8');
+        return $conn;
+    }
 
 }
 
--- scripts/db.sql
+++ scripts/db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE retreatin;
 
 CREATE USER 'retreatin'@'localhost' IDENTIFIED BY 'retreatin';
 
-GRANT ALL ON retreatinsourz.* TO 'retreatin'@'localhost';
+GRANT ALL ON retreatin.* TO 'retreatin'@'localhost';
 
 FLUSH PRIVILEGES;
 
